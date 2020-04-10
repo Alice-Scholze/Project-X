0ebccee045750e8b266a5fbfa1b20e45d70bb6a2
Alice Scholze <alicescholze@outlook.com>
2020-04-10 00:06:00 -0300

    bootstrap

---
+++ src/test/java/com/nois/projectx/ProjectXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nois.projectx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
